201c0971385a2d52b0b18e23337e997df4b23a0d
vaishali mukati <vaishalimukativasuki@gmail.com>
2023-09-25 18:38:49 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 24, 3, 61),
+              Color.fromARGB(255, 96, 19, 230)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World',style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),),
+          ),
+        ),
+      ),
+    ),
+  );
+}
